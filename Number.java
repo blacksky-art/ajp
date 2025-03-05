0e0860c98d046b46d9198960931a5a26a735204d
blacksky-art <allenjestin007@gmail.com>
2025-03-05 18:33:51 +0530

    Number.java

---
+++ Number.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public number
+{
+	public static void main(string[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		int g;
+		System.out.println("enter two numbers");
+		a = sc.NextInt();
+		b = sc.NextInt();
+		g = (a > b)? a : b;
+		System.out.println("greater number:" + g);
+		
+	}
+}
